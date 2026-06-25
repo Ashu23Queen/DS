2a0b92f591e58da15ffc10b86fec5cfaa0144b29
Ashwini <ashushindegit23@gmail.com>
2026-06-25 16:39:22 +0530

    main app updated with item

--- fastapi-model-deployment/using_python_typing_with_fastapi/main.py
+++ fastapi-model-deployment/using_python_typing_with_fastapi/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def home() -> dict[str, str]:
     return {"message": "Welcome to FastAPI with Typing"}
+
+@app.get("/items/{item_id}")
+def tead_item(item_id: int, q: str | None = None) -> dict[str, str | int | None]:
+    return {"item_id": item_id, "query": q}
