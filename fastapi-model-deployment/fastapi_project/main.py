17d51d70c2d176c596784c603074751dd03fd3e4
Ashwini <ashushindegit23@gmail.com>
2026-06-25 15:14:32 +0530

    main updated and api created

---
+++ fastapi-model-deployment/fastapi_project/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+ 
+app = FastAPI()
+ 
+@app.get("/") 
+def home() -> dict[str, str]:
+    return {"message": "Welcome to FastAPI with Typing"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None) -> dict[str, str | int | None]:
+    return {"item_id": item_id, "query": q}
