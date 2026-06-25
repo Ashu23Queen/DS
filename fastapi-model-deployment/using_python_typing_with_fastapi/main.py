861cc863bd8007ed8ae814e135a091a5c66eab1a
Ashwini <ashushindegit23@gmail.com>
2026-06-25 18:07:37 +0530

    Initial Commit: FastAPI using python typing model app file created

--- fastapi-model-deployment/using_python_typing_with_fastapi/main.py
+++ fastapi-model-deployment/using_python_typing_with_fastapi/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    in_stock: bool = True
 
 @app.get("/")
 def home() -> dict[str, str]:
@@ -9,3 +16,7 @@ def home() -> dict[str, str]:
 @app.get("/items/{item_id}")
 def tead_item(item_id: int, q: str | None = None) -> dict[str, str | int | None]:
     return {"item_id": item_id, "query": q}
+
+@app.post("items/")
+def create_item(item: Item) -> dict[str, Item]:
+    return {"item": item}
