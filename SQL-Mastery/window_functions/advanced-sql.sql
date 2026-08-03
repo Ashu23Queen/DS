8ec9a2c2b1681df4373f04ae318dc14b7b63ac7d
Ashwini <ashushindegit23@gmail.com>
2026-08-04 02:18:45 +0530

    median search frequency

--- SQL-Mastery/window_functions/advanced-sql.sql
+++ SQL-Mastery/window_functions/advanced-sql.sql
@@ -19,3 +19,18 @@ SELECT COUNT(merchant_id) AS payment_count
 FROM payments 
 WHERE minute_difference <= 10;
 
+
+# Median Google Search Frequency 
+/*
+Google’s Marketing Team needed to add a simple statistic to their upcoming Superbowl Ad: the median number of searches made per year. 
+You were given a summary table that tells you the number of searches made last year, write a query to report the median searches made per user.
+*/
+WITH expanded AS (
+  SELECT searches
+  FROM search_frequency, generate_series(1, num_users)
+)
+SELECT 
+  ROUND(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY searches)::DECIMAL, 1) AS median
+FROM expanded;
+
+
