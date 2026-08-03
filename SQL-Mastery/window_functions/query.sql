d857d3e20a90a536002053bce7f1ef33c6ca520e
Ashwini <ashushindegit23@gmail.com>
2026-08-03 18:27:43 +0530

    sum

--- SQL-Mastery/window_functions/query.sql
+++ SQL-Mastery/window_functions/query.sql
@@ -25,3 +25,27 @@ SELECT
     ROW_NUMBER() OVER (PARTITION BY Department ORDER BY Salary DESC) AS SalaryRank
 FROM Employees;
 
+
+# 2. RANK() and DENSE_RANK()
+/* Useful when you want to handle ties. RANK leaves gaps in ranking numbers if there's a tie, 
+whereas DENSE_RANK does not.
+*/
+SELECT 
+    EmployeeID, FirstName, LastName, Department, Salary,
+    RANK() OVER (ORDER BY Salary DESC) AS SalaryRank,
+    DENSE_RANK() OVER (ORDER BY Salary DESC) AS SalaryDenseRank
+FROM Employees;
+
+
+# 3. SUM()
+SELECT 
+    EmployeeID, FirstName, LastName, Department, Salary,
+    SUM(Salary) OVER (PARTITION BY Department) AS TotalDepartmentSalary,
+    SUM(Salary) OVER (ORDER BY Salary) AS RunningTotalSalary
+FROM Employees;
+
+
+
+
+
+
