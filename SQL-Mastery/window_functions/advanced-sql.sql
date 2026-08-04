77e6fa8a0c438e82bb2e7699e0aa150a91e8f8b1
Ashwini <ashushindegit23@gmail.com>
2026-08-04 12:48:15 +0530

    median search frequency

--- SQL-Mastery/window_functions/advanced-sql.sql
+++ SQL-Mastery/window_functions/advanced-sql.sql
@@ -33,6 +33,19 @@ SELECT
   ROUND(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY searches)::DECIMAL, 1) AS median
 FROM expanded;
 
+# another code
+WITH searches_expanded AS (
+  SELECT searches
+  FROM search_frequency
+  GROUP BY 
+    searches, 
+    GENERATE_SERIES(1, num_users))
+
+SELECT 
+  ROUND(PERCENTILE_CONT(0.50) WITHIN GROUP (
+    ORDER BY searches)::DECIMAL, 1) AS  median
+FROM searches_expanded;
+
 
 
 
